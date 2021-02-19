7f592cc486f961d096196ca3b103aa1d61fae665
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-19 08:50:48 +0300

    show more buton

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
   const [items, setItems] = useState([]);
   const [visible, setVisible] = useState(3);
   
+  const showMoreItems = () => {
+    setVisible(prevValue => prevValue + 3);
+  }
+
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(res => res.json())
@@ -21,7 +25,7 @@ function App() {
     <div className="App">
       <h1>Load More / Pagination</h1>
       <div className="container">
-        {items.map(item => (
+        {items.slice(0, visible).map(item => (
           <div className="card">
             <div className="id">
               <span>{item.id}</span>
@@ -29,6 +33,7 @@ function App() {
             <p>{item.body}</p>
           </div>
         ))}
+        <button onClick={showMoreItems} >Show More</button>
       </div>
     </div>
   );
