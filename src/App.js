55ad7d23adb3df3fb19f4114347aefe8d16feaae
Muhammed Fatih Candan <mfatihcandan@gmail.com>
2021-02-19 08:43:05 +0300

    fetch post

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [visible, setVisible] = useState(3);
+  
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(data => console.log(data));
+    return () => {
+      console.log("cleanup");
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Load More / Pagination</h1>
     </div>
   );
 }
